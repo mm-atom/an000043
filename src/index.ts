559bdb5621a4ca3bbcd0e35fb320d7e7f2b7977b
taoqf <tao_qiufeng@126.com>
2022-02-23 08:31:15 +0000

    add postfix to fileid

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { promises as fs } from 'fs';
+import { extname } from 'path';
 import anylogger from 'anylogger';
 import { Client, ClientOptions } from 'minio';
 import config from '@mmstudio/config';
@@ -43,10 +44,85 @@ export default async function up<M = Record<string, unknown>, N = Record<string,
 				// file may not exist. ignore
 			}
 		}
-		const id = file.id || uuid();
 		if (file.path) {
-			// 原文件，上传的时候有存储到文件系统中
-			// 压缩处理后的文件
+			const id = (() => {
+				if (file.id) {
+					return file.id;
+				}
+				const ext = extname(file.name);
+				if (ext) {
+					return `${uuid()}.${ext}`;
+				}
+				const id = uuid();
+				// 文件名中无后缀名
+				switch (file.type) {
+					case 'text/plain':
+						return `${id}.txt`;
+					case 'text/html':
+						return `${id}.html`;
+					case 'image/jpeg':
+						return `${id}.jpeg`;
+					case 'image/png':
+						return `${id}.png`;
+					case 'audio/wave':
+					case 'audio/wav':
+					case 'audio/x-wav':
+					case 'audio/x-pn-wav':
+						return `${id}.wav`;
+					case 'audio/mpeg':
+						return `${id}.mp3`;
+					case 'audio/ogg':
+						return `${id}.ogg`;
+					case 'video/mp4':
+						return `${id}.mp4`;
+					case 'video/ogg':
+					case 'application/ogg':
+						return `${id}.ogg`;
+					case 'application/json':
+						return `${id}.json`;
+					case 'application/javascript':
+						return `${id}.js`;
+					case 'application/ecmascript':
+						return `${id}.js`;
+					case 'image/gif':
+						return `${id}.gif`;
+					case 'image/svg+xml':
+						return `${id}.svg`;
+					case 'application/x-7z-compressed':
+						return `${id}.7z`;
+					case 'application/x-gzip':
+						return `${id}.gz`;
+					case 'application/zip':
+						return `${id}.zip`;
+					case 'application/x-rar':
+						return `${id}.rar`;
+					case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+						return `${id}.odt`;
+					case 'application/msword':
+						return `${id}.doc`;
+					case 'application/wps-office.docx':
+						return `${id}.docx`;
+					case 'application/vnd.ms-excel':
+						return `${id}.xls`;
+					case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+						return `${id}.ods`;
+					case 'application/wps-office.xlsx':
+						return `${id}.xlsx`;
+					case 'application/vnd.ms-powerpoint':
+						return `${id}.ppt`;
+					case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+						return `${id}.odp`;
+					case 'application/wps-office.pptx':
+						return `${id}.pptx`;
+					case 'application/pdf':
+						return `${id}.pdf`;
+					case 'audio/*':
+					case 'application/*':
+					case 'application/octet-stream':
+					default:
+						return id;
+				}
+			})();
 			const md5 = await client.fPutObject(NAME_SPACE, id, file.path, meta);
 			const doc = {
 				meta,
