ef39478ec502ab58a6fca78069a56d8016a061de
taoqf <tao_qiufeng@126.com>
2022-09-30 10:52:35 +0800

    add test

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,37 @@
 const test = require('ava');
+const { loadEnvConfig } = require('@next/env');
+const fs = require('fs/promises');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
+test.before(async () => {
+	loadEnvConfig('./', true);
+});
+
+const originFile = './tests/test.jpg';
+
+test('save to db', async (t) => {
+	const path = './tests/test1.jpg';
+	await fs.cp(originFile, path);
+	const file = {
+		fields: {},
+		name: 'test.jpg',
+		path,
+		type: 'image/jpeg'
+	};
+	await a([file], false);
+	t.pass();
+});
+
+test('encrypt', async (t) => {
+	const path = './tests/test2.jpg';
+	await fs.cp(originFile, path);
+	const file = {
+		fields: {},
+		name: 'test.jpg',
+		path,
+		type: 'image/jpeg'
+	};
+	await a([file], true);
 	t.pass();
 });
